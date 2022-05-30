88467cc00473875cfdc74f0347a07ab81b53d1a1
Vignesh KRV <vignesh1105krv@gmail.com>
2022-05-30 11:56:01 -0500

    handling events and shuffling array

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import numpy as np
+import random
 arr = np.array([10,20,30,40])
 pyscript.write('output4',f"my array is {arr}")
+
+def shuffle_array(*args):
+    shuffled=sorted(arr, key=lambda k:random.random())
+    pyscript.write('output4',f"shuffled array is {shuffled}")
