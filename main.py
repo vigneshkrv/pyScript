51c58d9eca6834e66c296a2d3d8b1031b79f7762
Vignesh KRV <vignesh1105krv@gmail.com>
2022-05-30 12:03:46 -0500

    Using Element() to access tags

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import numpy as np
 import random
+
+output_el = Element('output4').element
+
+#prints the element tag itself
+console.log(output_el)
+
 arr = np.array([10,20,30,40])
-pyscript.write('output4',f"my array is {arr}")
+# pyscript.write('output4',f"Original array is {arr}")  
+output_el.innerHTML=f"Original array is {arr}"
 
 def shuffle_array(*args):
     shuffled=sorted(arr, key=lambda k:random.random())
-    pyscript.write('output4',f"shuffled array is {shuffled}")
+    # pyscript.write('output4',f"Shuffled array is {shuffled}")
+    output_el.innerHTML=f"Shuffled array is {shuffled}"
