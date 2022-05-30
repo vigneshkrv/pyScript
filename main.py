9b7fd7685d950c63a749396fe829634f169805c8
Vignesh KRV <vignesh1105krv@gmail.com>
2022-05-30 12:09:13 -0500

    adding multiple files using paths in py-env

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from utils import add_class
 
 output_el = Element('output4').element
 
@@ -11,6 +12,10 @@ arr = np.array([10,20,30,40])
 output_el.innerHTML=f"Original array is {arr}"
 
 def shuffle_array(*args):
+    #shuffle array
     shuffled=sorted(arr, key=lambda k:random.random())
+
+    #chnage color using another .py file
+    add_class(output_el,"text-blue-500")
     # pyscript.write('output4',f"Shuffled array is {shuffled}")
     output_el.innerHTML=f"Shuffled array is {shuffled}"
